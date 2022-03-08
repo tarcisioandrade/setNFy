6a1f820852f011981b7cb3dafe45d683496bf020
Tarcisio Andrade <tarcisioandrade016@gmail.com>
2022-03-07 22:46:05 -0300

    react modal implementado

--- src/Components/ModalConfirm/ModalConfirm.js
+++ src/Components/ModalConfirm/ModalConfirm.js
@@ -1,18 +1,65 @@
-import React from 'react'
-import "./ModalConfirm.css"
+import React from "react";
+import Modal from "react-modal";
+import "./ModalConfirm.css";
+import { ReactComponent as AlertIcon } from "../../imgs/alert-outline.svg";
+import { useDispatch } from "react-redux";
 
-const ModalConfirm = () => {
+const customStyles = {
+  overlay: {
+    background: "rgba(0, 0, 0, 0.200)",
+  },
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    boxShadow: "0px 0px 5px 2px rgba(0,0,0,.5)",
+    borderRadius: "2px",
+  },
+};
+
+const ModalConfirm = ({ closeModal, toggleModal, finalize }) => {
+  const dispatch = useDispatch();
+  const { id, action, message } = finalize;
+
   return (
-    <div className='modal-confirm__content'>
-      <div className="modal-confirm">
-        <p>Remover Nota Fiscal?</p>
-        <div className="modal-confirm__buttons">
-          <button>Confirmar</button>
-          <button>Cancelar</button>
+    <>
+      <Modal
+        isOpen={toggleModal}
+        style={customStyles}
+        contentLabel="Modal de Confirmação"
+        onRequestClose={closeModal}
+        ariaHideApp={false}
+      >
+        <div className="modal-confirm">
+          <span>
+            <AlertIcon />
+            <p>{message}</p>
+          </span>
+          <div className="modal-confirm__buttons">
+            <button
+              className="modal-confirm_btn modal-confirm_btn--cancel"
+              onClick={closeModal}
+            >
+              Cancelar
+            </button>
+            <button
+              onClick={() => {
+                dispatch(action(id));
+                closeModal();
+              }}
+              className="modal-confirm_btn modal-confirm_btn--confirm"
+            >
+              Confirmar
+            </button>
+          </div>
         </div>
-      </div>
-    </div>
-  )
-}
+      </Modal>
+      ;
+    </>
+  );
+};
 
-export default ModalConfirm
+export default ModalConfirm;
