72a0f52bd1cb0f1bdaac3286ff0a6b6963cf7424
Tarcisio Andrade <tarcisioandrade016@gmail.com>
2022-06-03 17:43:43 -0300

    pacotes_instalados

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import App from "./App";
+import "antd/dist/antd.css";
+import { ConfigProvider } from "antd";
 import store from "./store/configureStore";
+import ptBR from "antd/es/locale/pt_BR";
 import ErrorBoundary from "./helper/ErrorBoundary";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <ErrorBoundary>
-        <App />
-      </ErrorBoundary>
-    </Provider>
+    <ConfigProvider locale={ptBR}>
+      <Provider store={store}>
+        <ErrorBoundary>
+          <App />
+        </ErrorBoundary>
+      </Provider>
+    </ConfigProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
